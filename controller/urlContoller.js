691f0479a4520ef40866ed476ef4326a7feb0bfa
Akhilesh Rana <akhileshrana079@gmail.com>
2026-06-21 08:38:57 +0530

    Add custom short code support

--- controller/urlContoller.js
+++ controller/urlContoller.js
@@ -4,7 +4,8 @@ const { nanoid } = require('nanoid');
 
 const shortenUrl = async (req, res) => {
   try {
-    const { originalUrl } = req.body;
+    const { originalUrl, customCode } = req.body;
+
     if (!originalUrl) {
       return res.status(400).json({
         message: "URL is required",
@@ -13,24 +14,39 @@ const shortenUrl = async (req, res) => {
 
     if (!validator.isURL(originalUrl, { require_protocol: true })) {
       return res.status(400).json({
-      message: "Please provide a valid URL starting with http:// or https://",
-  });
-}f
-    const shortCode = nanoid(6);
+        message: "Please provide a valid URL starting with http:// or https://",
+      });
+    }
+
+    let shortCode;
+
+    if (customCode) {
+      const existingUrl = await Url.findOne({ shortCode: customCode });
+
+      if (existingUrl) {
+        return res.status(409).json({
+          message: "This custom code is already taken",
+        });
+      }
+
+      shortCode = customCode;
+    } else {
+      shortCode = nanoid(6);
+    }
 
     const url = await Url.create({
       originalUrl,
       shortCode,
     });
 
-    res.status(201).json({
-    message: "Short URL created successfully",
-    shortUrl: `${req.protocol}://${req.get("host")}/${shortCode}`,
-    originalUrl: url.originalUrl,
-    shortCode: url.shortCode,
+    return res.status(201).json({
+      message: "Short URL created successfully",
+      shortUrl: `${req.protocol}://${req.get("host")}/${shortCode}`,
+      originalUrl: url.originalUrl,
+      shortCode: url.shortCode,
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       message: error.message,
     });
   }
